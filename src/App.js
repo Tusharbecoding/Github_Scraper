22a4e2a5f514e49cf4400431158d71d51d4892c2
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-05-12 17:19:01 +0530

    added github api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from './components/layouts/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
-        <Navbar title='Github Scraper' />
+        <Navbar title="Github Scraper" />
 
-        <main>
-          Content
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
         </main>
+
+        <Footer />
       </div>
     </Router>
   );
