576b950defb89513bae0c82309ed7beee23c7724
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-05-12 18:05:14 +0530

    started with contexts

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "../layouts/Spinner";
+import UserItem from "./UserItem";
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -25,12 +27,12 @@ const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   } else {
-    return <h3>Loading</h3>;
+    return <Spinner />;
   }
 };
 
