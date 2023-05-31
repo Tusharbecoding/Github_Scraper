fcf1375f3c1c7b09ec206cba86271d1adad86986
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-05-31 20:13:14 +0530

    added dispatch routines and actions

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,89 +16,12 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(
-      `${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,
-      {
-        headers: {
-          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    const { items } = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(
-      `${process.env.REACT_APP_GITHUB_URL}/users/${login}`,
-      {
-        headers: {
-          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    if (response.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    const response = await fetch(
-      `${process.env.REACT_APP_GITHUB_URL}/users/${login}/repos`,
-      {
-        headers: {
-          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    dispatch({
-      type: "GET_REPOS",
-      payload: data,
-    });
-  };
-
-  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
-
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
-        repos: state.repos,
-        getUser,
-        getUserRepos,
+        ...state,
+
+        dispatch,
       }}
     >
       {children}
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,25 +1,28 @@
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useEffect, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
-import Spinner from "../components/layout/Spinner";
+import Spinner from "../components/layouts/Spinner";
 import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
-import { getUserAndRepos } from "../context/github/GithubActions";
+import { getUser, getUserRepos } from "../context/github/GithubActions";
 
 const User = () => {
-  const { user, loading, getUserRepos } = useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
     dispatch({ type: "SET_LOADING" });
     const getUserData = async () => {
-      const userData = await getUserAndRepos(params.login);
-      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+      const userData = await getUser(params.login);
+      dispatch({ type: "GET_USER", payload: userData });
+
+      const userRepoData = await getUserRepos(params.login);
+      dispatch({ type: "GET_REPOS", payload: userRepoData });
     };
 
     getUserData();
-  }, [dispatch, params.login]);
+  }, []);
 
   const {
     name,
